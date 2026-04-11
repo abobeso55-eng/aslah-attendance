7103c20039a0f76c5ef4cd4e3aa43562638dc848
abobeso55-eng <abobeso55@gmail.com>
2026-04-12 00:35:57 +0300

    Create index.js

---
+++ api/getPing/index.js
@@ -0,0 +1,10 @@
+module.exports = async function (context, req) {
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: {
+      ok: true,
+      message: "API is working"
+    }
+  };
+};
